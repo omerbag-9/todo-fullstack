71c2b6322683840d54722eca3e54e033cb2c0e6d
omerbag-9 <omerbagprog@gmail.com>
2024-09-06 01:55:22 +0300

    change db link

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 export const connectDB = () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/todoFullStack').then(()=>{
+    mongoose.connect('mongodb+srv://omerbagprog:Lx6X9u7ZyeJtB87a@cluster0.89df3.mongodb.net/todoFullStack').then(()=>{
         console.log('db connected successfully');
     })
 }
