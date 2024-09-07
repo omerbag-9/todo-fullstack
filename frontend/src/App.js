9151221d6c4b3aa60ff37850cd6fe4e956cb576f
omerbag-9 <omerbagprog@gmail.com>
2024-09-08 02:20:00 +0300

    feat signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
+import Signup from './components/signup/Signup';
+import Login from './components/login/Login';
 
+
+let routers = createBrowserRouter([
+  {path:"/",element:<Layout/>,children:[
+    {index:true,element:<Home/>},
+    {path:"/signup",element:<Signup/>},
+    {path:"/login",element:<Login/>},
+  ]}
+])
 function App() {
   return (
-    <h2>hello</h2>
+    <RouterProvider router={routers}></RouterProvider>
   );
 }
 
