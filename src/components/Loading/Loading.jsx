bb8ac6dd041b8c9dcff86b876bb57feb3aa1551c
omerbag-9 <omerbagprog@gmail.com>
2024-09-12 21:37:03 +0300

    production

--- src/components/Loading/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import logo from '../../assets/images/logo.png'
-export default function Loading() {
-  return <>
-    <div className="loading d-flex justify-content-center align-items-center position-fixed top-0 end-0 bottom-0 start-0 ">
-      <img src={logo} width={300} alt="" />
-    </div>
-  </>
-}
