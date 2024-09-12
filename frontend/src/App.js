084a902537dc4b2fafa04fc9f151f20e250d7859
omerbag-9 <omerbagprog@gmail.com>
2024-09-12 21:03:27 +0300

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
+import Signup from './components/signup/Signup';
+import Login from './components/login/Login';
+import { useContext, useEffect } from 'react';
+import { UserContext } from './context/UserContext';
+import ProtectedRoute from './components/protectedRoute/ProtectedRoute';
+
+
+let routers = createHashRouter([
+  {
+    path: "/", element: <Layout />, children: [
+      { index: true, element: <ProtectedRoute><Home /></ProtectedRoute> },
+      { path: "signup", element: <Signup /> },
+      { path: "login", element: <Login /> },
+    ]
+  }
+])
+function App() {
+
+  const { setUserToken } = useContext(UserContext)
+  useEffect(() => {
+    const token = localStorage.getItem('userToken');
+    if (token) {
+      setUserToken(token);
+    }
+  }, []);
+
+  return (
+    <RouterProvider router={routers}></RouterProvider>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserContext } from '../../context/UserContext'
+import logo from '../../assets/images/logo.png'
+export default function Navbar() {
+  let naviagte = useNavigate();
+  let { userToken, setUserToken } = useContext(UserContext)
+  function logout() {
+    localStorage.removeItem('userToken')
+    setUserToken(null)
+  }
+  useEffect(() => {
+    if (!userToken) {
+      naviagte('/login')
+    }
+  }, [userToken, naviagte])
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-teritary pt-2">
+        <div className="container">
+          <Link className="navbar-brand text-primary" to="/">
+            <img src={logo} width={40} alt="" />
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          </div>
+        </div>
+        <ul className="navbar-nav d-flex ms-auto mb-2 mb-lg-0">
+          {userToken ? <><li className="nav-item">
+            <Link onClick={() => logout()} className="nav-link active bg-primary rounded  me-2 text-white" aria-current="page">logout</Link>
+          </li></> : <><li className="nav-item">
+            <Link className="nav-link active bg-primary rounded me-2 text-white" aria-current="page" to="/signup">signup</Link>
+          </li>
+            <li className="nav-item">
+              <Link className="nav-link active bg-primary rounded me-2 text-white" aria-current="page" to="/login">login</Link>
+            </li>
+          </>}
+
+        </ul>
+      </nav>
+    </>
+  )
+}
